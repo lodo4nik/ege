37d33b8dea387fd9d67d9cc03a23ed6affe13720
lodo4nik <rompetaalex@gmail.com>
2026-02-19 19:52:17 +0300

    починил

--- 15/feb_20_2.py
+++ 15/feb_20_2.py
@@ -1,14 +1,12 @@
 for a in range(1, 200):
     flag = True
-    for x in range(1, 100):
-        for y in range(1, 100):
-            if not(((x % 30 == 0) and (x % 45 != 0)) <= (x % a != 0)):
-                flag = False
-                break
-            if not flag:
-                break
+    for x in range(1, 10000):
+        if not(((x % 30 == 0) and (x % 45 != 0)) <= (x % a != 0)):
+            flag = False
+            break
     if flag:
         print(a)
+        break
 
 
-## НЕВЕРНО НЕВЕРНО НЕВЕРНО
+## ВЕРНО ВЕРНО ВЕРНО
